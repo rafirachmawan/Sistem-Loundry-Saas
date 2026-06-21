62f7801ee76118b47af733d0f8d91b4e3e8098cb
rafirachmawan <rafirachmawan1987@gmail.com>
2026-06-21 16:28:52 +0700

    feat: implement WhatsApp gateway microservice using whatsapp-web.js and configure project ignore patterns

--- wa-gateway/index.js
+++ wa-gateway/index.js
@@ -25,6 +25,10 @@ const client = new Client({
             '--no-zygote',
             '--disable-gpu'
         ]
+    },
+    webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
     }
 });
 
